8199d6c75eefb7d2f662b9285c76ffcd0e3f8c3a
mnfitz-ASU <mnfitzg1@asu.edu>
2023-11-02 12:27:47 -0700

    Builds, but doesn't gen dll files

---
+++ dllmain.cpp
@@ -0,0 +1,45 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include <winnt.h>
+#include <minwindef.h>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+
+/*
+
+#include "stdafx.h"
+#include <windows.h>
+#include <stdio.h>
+
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		MessageBox(NULL, L"DllMain loaded", L"Success", 0);
+	case DLL_PROCESS_DETACH:
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	}
+	return TRUE;
+}
+*/
+
